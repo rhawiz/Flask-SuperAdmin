8183113ec0f33d2afa719aa6265048cd2dc27c73
Rawand Hawiz <r.hawiz@geophy.com>
2018-01-24 12:07:02 +0100

    bug in print

--- flask_superadmin/babel.py
+++ flask_superadmin/babel.py
@@ -12,7 +12,7 @@ try:
                   domain='admin')
 
         def get_translations_path(self, ctx):
-            print ctx
+            print(ctx)
 
             dirname = ctx.app.extensions['admin'].translations_path
             if dirname is not None:
